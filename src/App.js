3045769557a45d9781d2f120c761acd9cebfa29b
olitt <erickochieng230@gmail.com>
2020-12-26 15:22:53 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+import { UserList } from './Components/users';
+import { PostList, PostEdit, PostCreate, PostShow} from './Components/posts';
+import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/People';
+import MessageIcon from '@material-ui/icons/Message';
+import Dashboard from './Components/Dashboard';
+import authProvider from './Components/authProvider';
+import {CommentList, CommentEdit} from './Components/comments';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark', // Switching the dark mode on is a single property value change.
+  },
+});
+
+const App = () => (
+        <Admin theme={theme} dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+          <Resource name="users" list={UserList} icon={UserIcon} />
+          <Resource name="posts" options={{ label: 'Posts' }} list={PostList} show={PostShow} edit={PostEdit}  create={PostCreate}  icon={PostIcon} />
+          <Resource name="comments" list={CommentList} icon={MessageIcon} edit={CommentEdit}  />
+        </Admin>
+);
 
 export default App;
