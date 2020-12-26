b8acc9c9ff62805697ce24cd38da7dd68187a676
ErickNyagilo <55799728+ErickNyagilo@users.noreply.github.com>
2020-12-26 20:06:58 +0300

    fix: removing the theme component

--- src/App.js
+++ src/App.js
@@ -9,19 +9,15 @@ import MessageIcon from '@material-ui/icons/Message';
 import Dashboard from './Components/Dashboard';
 import authProvider from './Components/authProvider';
 import {CommentList, CommentEdit} from './Components/comments';
-import { createMuiTheme } from '@material-ui/core/styles';
+
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark', // Switching the dark mode on is a single property value change.
-  },
-});
+
 
 const App = () => (
-        <Admin theme={theme} dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+        <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
           <Resource name="users" list={UserList} icon={UserIcon} />
           <Resource name="posts" options={{ label: 'Posts' }} list={PostList} show={PostShow} edit={PostEdit}  create={PostCreate}  icon={PostIcon} />
           <Resource name="comments" list={CommentList} icon={MessageIcon} edit={CommentEdit}  />
